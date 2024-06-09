c6db378378737b153fb5664763a2c5728d336426
pljha <caiomarronegomes@gmail.com>
2024-06-09 12:31:30 -0300

    Update xota.js

--- xota.js
+++ xota.js
@@ -1,43 +1,4 @@
-        const destaqueContainer = document.querySelector('.destaque');
-        const larguraTela = window.innerWidth;
-
-        if (larguraTela > 0 && larguraTela < 700) {
-            // Verifica se a largura da tela está entre 200px e 400px
-            const ultimaDiv = document.createElement('div');
-            ultimaDiv.classList.add('imagem');
-
-            const img = document.createElement('img');
-            img.src = "cricketleague.png";
-            img.alt = "Descrição da imagem";
-
-            const legendaDiv = document.createElement('div');
-            legendaDiv.classList.add('legenda');
-            legendaDiv.textContent = "CRICKET LEAGUE";
-
-            ultimaDiv.appendChild(img);
-            ultimaDiv.appendChild(legendaDiv);
-            destaqueContainer.appendChild(ultimaDiv);
-        } else {
-            const lancamentoContainer = document.querySelector('.lancamento');
-            const h1Element = lancamentoContainer.querySelector('h1');
-            
-            const novaDiv = document.createElement('div');
-            novaDiv.classList.add('imagem');
-
-            const img = document.createElement('img');
-            img.src = "golfbattlemini.png"; // Substitua "nova-imagem.png" pelo caminho da sua nova imagem
-            img.alt = "Descrição da nova imagem";
-
-            const legendaDiv = document.createElement('div');
-            legendaDiv.classList.add('legenda');
-            legendaDiv.textContent = "NOVO LANÇAMENTO";
-
-            novaDiv.appendChild(img);
-            novaDiv.appendChild(legendaDiv);
-            
-            // Adiciona a nova div após o h1 na classe .lancamento
-            lancamentoContainer.insertBefore(novaDiv, h1Element.nextSibling);
-        }
+        
 
         function toggleMenu(x) {
             x.classList.toggle("change");
